ca359b5b8288c3753e84429487c9d7f2fba113b1
Prakhar Dwivedi <prakhar.dwivedi.7707@gmail.com>
2026-03-12 23:42:32 +0530

    fixed bugs

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api/', 
+    baseURL: 'https://pulse-v2-backend.onrender.com/api/', 
 });
 
 // 1. Attaches the token to every request
